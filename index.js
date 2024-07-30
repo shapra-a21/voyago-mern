c87f14e9039a670df1ae375fddb726fc8df299c8
shapra-a21 <shafapradiva00@gmail.com>
2024-07-30 17:00:04 +0700

    test

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 const corsOptions = {
-    httpOnly: true,
     secure: false, 
     sameSite: "lax",    
     origin: true,
